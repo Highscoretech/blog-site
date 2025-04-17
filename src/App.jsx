d5bf94172a153eda7800dcd6df2754b857fbcecb
Highscoretech <highscoretechng@gmail.com>
2025-04-17 19:40:37 +0100

    slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './component/Navbar';
+import Home from './page/Home';
 
 function App() {
   return (
     <>
     <Navbar />
-
-      {/* <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </Router> */}
+    <div className='app-container'>
+      <div className="app-section">
+        <Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              {/* <Route path="/about" element={<About />} /> */}
+            </Routes>
+          </Router>
+      </div>
+    </div>
     </>
   )
 }
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules'; // Import Autoplay
+import 'swiper/css';
 
 export default function Home() {
   return (
-    <div className='home'>
-        
+    <div className='home'> 
+      <Swiper
+        loop={true}
+        modules={[Autoplay]}
+        spaceBetween={15} // Add some space to see transitions better
+        slidesPerView={3} // Start with one slide visible
+        autoplay={{
+            delay: 2000, // Adjust the delay as needed (milliseconds)
+            disableOnInteraction: false, // Important: Allow swiping without stopping autoplay
+        }}
+        speed={1200}
+        breakpoints={{
+            // Optional: Responsive breakpoints
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 15,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 15,
+            },
+          }}
+      >                
+      <SwiperSlide >
+          <div className="slide-img">
+            <img src="/assets/slide1.jpeg" alt="" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide >
+            <div className="slide-img">
+                <img src="/assets/download.jpg" alt="" />
+            </div>
+        </SwiperSlide>
+        <SwiperSlide >
+            <div className="slide-img">
+                <img src="/assets/images (2).jpg" alt="" />
+            </div>
+        </SwiperSlide>
+        <SwiperSlide >
+            <div className="slide-img">
+                <img src="/assets/slider-revolution-banner-1024x433.png" alt="" />
+            </div>
+        </SwiperSlide>
+        <SwiperSlide >
+            <div className="slide-img">
+                <img src="/assets/images.jpg" alt="" />
+            </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
-  )
+  );
 }
